e95bbcfbe3e97a66fdcf3edd4bd1ffd49c3d9ef4
Brandon Browning <brandonbrowning@live.com>
2012-12-16 20:49:41 -0500

    Working on basic parsing strategy

---
+++ ChickenSalad.SeniorHonors/ChickenSalad.SeniorHonors/Main.fs
@@ -0,0 +1,21 @@
+﻿
+module Main
+open System
+open FParsec
+
+type UserState = unit
+type Parser<'t> = Parser<'t, UserState>
+
+let test p str =
+    match run p str with
+        | Success(result, _, _)   -> printfn "Success: %A" result
+        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+let floatsInBrackets: Parser<float list> = between (pstring "[") (pstring "]") pfloat |> many
+let floatsWithCommas: Parser<float list> = pstring "[" >>. sepBy pfloat (pstring "," .>> spaces) .>> pstring "]"
+
+[<EntryPoint>]
+let main args =
+    test floatsInBrackets "[1][2]"
+    test floatsWithCommas "[1, 2]"
+    0
