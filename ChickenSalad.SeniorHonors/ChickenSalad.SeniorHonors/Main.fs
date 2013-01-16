6c58c63c1c04847d1e33f3cc00c30fa7057816fe
Brandon Browning <brandonbrowning@live.com>
2013-01-16 10:14:13 -0500

    Added the grammar, and other changes I forgot

--- ChickenSalad.SeniorHonors/ChickenSalad.SeniorHonors/Main.fs
+++ ChickenSalad.SeniorHonors/ChickenSalad.SeniorHonors/Main.fs
@@ -2,16 +2,15 @@
 module Main
 open System
 open FParsec
-open ProtoSql
-
-let test p str =
-    match run p str with
-        | Success(result, _, _)   -> printfn "Success: %A" result
-        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+open Basic
 
 [<EntryPoint>]
 let main args =
-    test parser "foo(bar(5, -42.01, 'rawr'), 1)"
+    let result = run pfoo "x = foo(5\r\ny = x\r\nprint(y)"
+
+    match result with
+        | Success(result, _, _)   -> printfn "Success: %A" result
+        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
     Console.ReadLine() |> ignore
     0
--- ChickenSalad.SeniorHonors/ChickenSalad.SeniorHonors/ProtoSql.fs
+++ ChickenSalad.SeniorHonors/ChickenSalad.SeniorHonors/ProtoSql.fs
@@ -3,44 +3,7 @@ module ProtoSql
 open System
 open FParsec
 open Parsing
+open ProtoSqlValue
 
-type ProtoSql =
-    | ProtoSqlLiteral of string
-    | ProtoSqlString of string
-    | ProtoSqlInteger of int
-    | ProtoSqlFloat of float
-    | ProtoSqlFunctionCall of string * ProtoSql list
-    
-type ProtoSqlParser = Parser<ProtoSql, unit>
-
-let parser, parserRef = createParserForwardedToRef()
-
-let parseRawIdentifier: Parser<string, unit> = many1Satisfy2 isLetter (fun c -> isLetter c || isDigit c)
-let parseRawString = pchar ''' >>. manySatisfy (fun c -> c <> ''') .>> pchar '''
-let parseRawInteger = pint32
-let parseRawFloat = pfloat
-
-let parseLiteral = parseRawIdentifier |>> ProtoSqlLiteral
-let parseString = parseRawString |>> ProtoSqlString
-let parseInteger: ProtoSqlParser = parseRawInteger |>> ProtoSqlInteger
-let parseFloat: ProtoSqlParser = parseRawFloat |>> ProtoSqlFloat
-
-let parseNumberOptions = NumberLiteralOptions.AllowFraction ||| NumberLiteralOptions.AllowMinusSign
-let parseNumber = 
-    numberLiteral parseNumberOptions "number"
-        |>> fun x ->
-            if x.IsInteger then x.String |> int |> ProtoSqlInteger
-            else x.String |> float |> ProtoSqlFloat
-
-let parseFunctionCall =
-    let argumentCsv = csv parser
-    let funcCall = betweenStr "(" ")" argumentCsv
-
-    parseRawIdentifier .>>. funcCall |>> ProtoSqlFunctionCall
-
-parserRef := choice [
-    parseFunctionCall;
-    parseString;
-    parseNumber;
-    parseLiteral
-]
+// type SelectColumnExpression = string * ProtoSqlValue
+// type SelectExpression = 
